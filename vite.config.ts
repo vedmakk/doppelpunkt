bda535ad2f7e43eab69c7e1db0066c6d427ed24e
vedmakk <jan.mittelman@proton.me>
2025-06-03 11:20:12 +0300

    Cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
+  define: {
+    'import.meta.env.VITE_APP_VERSION': JSON.stringify(
+      process.env.npm_package_version,
+    ),
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 })
